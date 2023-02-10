907ed0addfdba6f17aabac670c5473516298a94b
Tyler See <72323540+tylers122@users.noreply.github.com>
2023-02-10 04:37:07 -0800

    Added getAlgo(), finished menu
    getAlgo() determines which algorithm to used based on user input.
    Cleaned up all functions and finished menu of program.

--- puzzle.py
+++ puzzle.py
@@ -4,45 +4,55 @@ import time
 #0 represents empty tile
 goalState = ((1, 2 ,3), (4, 5, 6), (7, 8, 0))
 
+#cleaned up main function/put everything together
 def main():
-    print("Welcome to the 8 Puzzle solver!")
-    print("Choose an algorithm (1, 2, 3):")
-    print("1. Uniform Cost Search")
-    print("2. A* with Misplaced Tile Heuristic")
-    print("3. A* with Euclidean Distance Heuristic")
-    alg = input()
+    print("Welcome to the 8 Puzzle solver!\n")
+    puzzle = getPuzzle()
+    algo = getAlgo()
+
 
 #gets a default puzzle or lets user create custom puzzle
 def getPuzzle():
-    finish = False
-    print("Select a puzzle:")
-    print("1 - Premade Puzzle")
-    print("2 - Custom Puzzle")
-    choice = int(input())
-
-    while not finish:
-        if choice == 1:
-            return defaultPuzzles()
-        elif choice == 2:
-            return createPuzzle()
+    while True:
         print("Select a puzzle:")
         print("1 - Premade Puzzle")
         print("2 - Custom Puzzle")
         choice = int(input())
-    
-#default puzzles based on user input difficulty
-def defaultPuzzles():
+
+        if choice == 1:
+            return preMadePuzzles()
+        elif choice == 2:
+            return createPuzzle()
+
+#determines which algorithm to used based on user input
+def getAlgo():
+    while True:
+        print("Choose an algorithm (1, 2, 3):")
+        print("1. Uniform Cost Search")
+        print("2. A* with Misplaced Tile Heuristic")
+        print("3. A* with Euclidean Distance Heuristic")
+        alg = int(input())
+
+        if alg == 1:
+            return uniformCost()
+        elif alg == 2:
+            return misplacedTile()
+        elif alg == 3:
+            return euclidian()
+
+
+#premade puzzles based on user input difficulty
+def preMadePuzzles():
     puzzles = (((1, 2, 3), (4, 5, 6), (7, 8, 0)),   #trivial
                ((1, 2, 3), (4, 5, 6), (7, 0, 8)),   #very easy
                (())) #add more later
     
-    level = int(input("Enter a difficulty level from 1 - 9 (easy - hard): "))
-    finish = False
-    while not finish:
+    while True:
+        level = int(input("Enter a difficulty level from 1 - 9 (easy - hard): "))
         if level >= 1 and level <= 9:
             printPuzzle(puzzles[level - 1])
             return puzzles[level - 1]
-        level = int(input("Enter a difficulty level from 1 - 9 (easy - hard): "))
+            
 
 def createPuzzle():
     print("Enter a puzzle! Use 0 for a blank space.")
@@ -60,9 +70,8 @@ def createPuzzle():
 
     userPuzzle = tuple(userPuzzle)  #convert list to tuple, now unchangeable
     printPuzzle(userPuzzle)
-    return userPuzzle        
-        
-        
+    return userPuzzle
+
 #prints puzzle
 def printPuzzle(puzzle):
     result = ""
@@ -70,10 +79,9 @@ def printPuzzle(puzzle):
         result += str(puzzle[i]) + "\n"
     
     print(result)
+        
 
-    
-    
-    
+
 #heuristic functions
 def uniformCost():
     #no heuristic, return 0
@@ -81,10 +89,10 @@ def uniformCost():
 
 def misplacedTile():
     pass
+    
 
 def euclidian():
     pass
 
 
-# main()
-getPuzzle()
+main()
